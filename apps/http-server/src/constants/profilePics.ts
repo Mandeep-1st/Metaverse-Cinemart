8887fa284242a9014692e1a9a9fb5c9169507d4c
Mandeep Sharma <mandeepsharma0701@gmail.com>
2026-01-27 12:46:31 +0530

    add default profile pics

---
+++ apps/http-server/src/constants/profilePics.ts
@@ -0,0 +1,8 @@
+export const randomProfilePics = [
+    "https://res.cloudinary.com/democloudinarymandeep/image/upload/v1769490168/artby-hensi-tTcZ607X66Q-unsplash_d1yh1j.jpg",
+    "https://res.cloudinary.com/democloudinarymandeep/image/upload/v1769490166/artby-hensi-dN-R9royffU-unsplash_podg2m.jpg",
+    "https://res.cloudinary.com/democloudinarymandeep/image/upload/v1769490166/artby-hensi-m5d161npGl4-unsplash_dxvrst.jpg",
+    "https://res.cloudinary.com/democloudinarymandeep/image/upload/v1769490166/artby-hensi-aK8QNbvWxd0-unsplash_mxzbew.jpg",
+    "https://res.cloudinary.com/democloudinarymandeep/image/upload/v1769490166/artby-hensi-F3CPPVp2U5c-unsplash_mnxylo.jpg",
+    "https://res.cloudinary.com/democloudinarymandeep/image/upload/v1769490166/artby-hensi-ELsHxUox7OU-unsplash_haqlke.jpg"
+]
