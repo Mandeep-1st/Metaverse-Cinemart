62aab73517445d02c73e9c0a98285e11f76c5bcf
Mandeep Sharma <mandeepsharma0701@gmail.com>
2026-04-01 19:15:41 +0530

    Changing the configuration type of the redis

--- apps/http-server/src/utils/queue.ts
+++ apps/http-server/src/utils/queue.ts
@@ -1,12 +1,10 @@
 import Redis from "ioredis";
 
-const REDIS_HOST = process.env.SERVER_VAR_REDIS_HOST || "127.0.0.1";
-const REDIS_PORT = Number(process.env.SERVER_VAR_REDIS_PORT) || 6379;
+const REDIS_URL = process.env.SERVER_VAR_REDIS_URL;
+// const REDIS_HOST = process.env.SERVER_VAR_REDIS_HOST || "127.0.0.1";
+// const REDIS_PORT = Number(process.env.SERVER_VAR_REDIS_PORT) || 6379;
 
-const redis = new Redis({
-    host: REDIS_HOST,
-    port: REDIS_PORT,
-});
+const redis = new Redis(REDIS_URL || "redis://127.0.0.1:6379");
 
 export const sendToQueue = async (queueName: string, data: any) => {
     try {
